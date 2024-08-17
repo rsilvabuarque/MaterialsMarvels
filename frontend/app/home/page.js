fc2dbc39af22e1c1784aa84c51b10699a7c45c9f
Raunak Mondal <spmondal08@gmail.com>
2024-08-17 16:07:34 -0700

    Update page.js

--- frontend/app/home/page.js
+++ frontend/app/home/page.js
@@ -9,40 +9,48 @@ import Col from 'react-bootstrap/Col';
 import { useEffect, useState } from 'react';
 
 const HomePage = () => {
-  const [htmlContent, setHtmlContent] = useState('');
+    const [htmlContent, setHtmlContent] = useState('');
 
-  useEffect(() => {
-    const fetchHtml = async () => {
-      try {
-        const response = await fetch('/api/getfile/visualization.html');
-        if (response.ok) {
-          const resp = await response.json();
-          setHtmlContent(resp.content);
-        } else {
-          console.error('Failed to fetch HTML content');
-        }
-      } catch (error) {
-        console.error('Error fetching HTML content:', error);
-      }
-    };
+    useEffect(() => {
+        const fetchHtml = async () => {
+            try {
+                const response = await fetch('/api/getfile/visualization.html');
+                if (response.ok) {
+                    const resp = await response.json();
+                    setHtmlContent(resp.content);
+                } else {
+                    console.error('Failed to fetch HTML content');
+                }
+            } catch (error) {
+                console.error('Error fetching HTML content:', error);
+            }
+        };
 
-    fetchHtml();
-  }, []);
+        fetchHtml();
+    }, []);
 
-  return (
-    <>
-      <Container>
-        <Row>
-          <Col style={{ "borderColor": "navy", "borderStyle": "solid" }}><Visualization markup={htmlContent}></Visualization></Col>
-          <Col style={{ "borderColor": "navy", "borderStyle": "solid" }}><EnergyPlot /></Col>
-        </Row>
-        <Row>
-          <Col style={{ "borderColor": "navy", "borderStyle": "solid" }}><div>Explanation text <br /> Explanation text</div></Col>
-          <Col style={{ "borderColor": "navy", "borderStyle": "solid" }}><EnergyPlot /></Col>
-        </Row>
-      </Container>
-    </>
-  )
+    return (
+        <>
+            <Container className={styles.pageContainer}>
+                <Row>
+                    <Col className={styles.visualizationCol}>
+                        <Visualization markup={htmlContent} />
+                    </Col>
+                    <Col className={styles.plotCol}>
+                        <EnergyPlot />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className={styles.textCol}>
+                        <div className={styles.explanationText}>Explanation text <br /> Explanation text</div>
+                    </Col>
+                    <Col className={styles.plotCol}>
+                        <EnergyPlot />
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    )
 }
 
 export default HomePage;
