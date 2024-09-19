a242d411855387db8f3db17e9b7dd806e70934ff
Sachin Ramanathan <sachinr.exe@gmail.com>
2024-09-19 09:23:45 +0800

    Configure environments in next.config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,21 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    rewrites: async () => {
-        return [
-          {
-            source: '/api/:path*',
-            destination: 'http://127.0.0.1:8000/api/:path*',
-            // destination:
-            //   process.env.NODE_ENV === 'development'
-            //     ? 'http://127.0.0.1:7000/api/:path*'
-            //     : '/api/',
-          },
-        ]
+  rewrites: async () => {
+    const isProduction = process.env.NODE_ENV === 'production';
+    const isStaging = process.env.NODE_ENV === 'staging';
+    
+    // Default to the production API for all environments unless overridden
+    let apiUrl = 'http://127.0.0.1:8000'; // Production API URL
+
+    if (isStaging) {
+      apiUrl = 'http://127.0.0.1:8001'; // Staging API URL
+    }
+
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${apiUrl}/api/:path*`, // Forward API requests
       },
-      // To ignore error useSearchParams() should be wrapped in a suspense boundary at page "/visualization"
-      experimental: { missingSuspenseWithCSRBailout: false, },
-      // For Docker
-      output: "standalone",
+    ];
+  },
+  // To ignore error: useSearchParams() should be wrapped in a suspense boundary at page "/visualization"
+  experimental: { 
+    missingSuspenseWithCSRBailout: false, 
+  },
+  // For Docker
+  output: "standalone",
+  // Base URL for frontend app, used for setting custom environment variables
+  env: {
+    BASE_API_URL: process.env.NODE_ENV === 'staging' ? 'http://127.0.0.1:8001' : 'http://127.0.0.1:8000',
+  }
 };
 
 export default nextConfig;
