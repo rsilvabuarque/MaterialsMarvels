26ced1f58efeaccb24c497c99a5f7442f6beec09
Sachin Ramanathan <sachinr.exe@gmail.com>
2024-09-26 10:34:18 -0700

    Add favicon and logo

--- frontend/public/static/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-      var stage = new NGL.Stage("viewport");
-      stage.loadFile("rcsb://1crn", {defaultRepresentation: true}).then(function (component) {
-            // add a "cartoon" representation to the structure component
-            component.addRepresentation("cartoon");
-            // provide a "good" view of the structure
-            component.autoView();
-          });
-});
