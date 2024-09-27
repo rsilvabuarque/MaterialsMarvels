569bf14d59940b4cff39021459f7d060a0ef241e
Sachin Ramanathan <sachinr.exe@gmail.com>
2024-09-26 22:15:46 -0700

    Enhancement: Loading button

--- frontend/app/ionic-bonding/sendMolData.js
+++ frontend/app/ionic-bonding/sendMolData.js
@@ -1,20 +1,21 @@
-function sendMolData() {
-  window.ketcher.getMolfile('v2000').then((molfile) => {
-    // Send the molfile to the Flask API and retrieve the output after computation
-    fetch('/api/visualize', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ 'molfile': molfile }),
-    }).then((response) => {
-      return response.json();
-    }).then((data) => {
-      // Simulation has been completed - redirect to the visualization page
-      window.location.href = '/visualization?visualId=' + data.visualId;
-    })
+async function sendMolData() {
+  // Get the molfile from Ketcher
+  const molfile = await window.ketcher.getMolfile('v2000');
+
+  // Send the molfile to the Flask API and wait for the response
+  const response = await fetch('/api/visualize', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ 'molfile': molfile }),
   });
-  return new Promise((resolve) => setTimeout(resolve, 1000));
+
+  // Parse the JSON response
+  const data = await response.json();
+
+  // Redirect to the visualization page after simulation completed
+  window.location.href = '/visualization?visualId=' + data.visualId;
 }
 
 export default sendMolData;
