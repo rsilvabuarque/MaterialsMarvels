e097ffe693bd5ec2ec1ec69705a7683edebc1839
Sachin Ramanathan <sachinr.exe@gmail.com>
2024-10-10 09:43:51 -0700

    Add info card for ionic bonding

--- frontend/app/ionic-bonding/page.js
+++ frontend/app/ionic-bonding/page.js
@@ -3,6 +3,8 @@
 import 'ketcher-react/dist/index.css';
 import './page.module.css';
 import { useEffect, useState } from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 import LoadingButton from './LoadingBtn';
 import Navigation from '../components/Navigation';
 import dynamic from "next/dynamic";
@@ -26,6 +28,27 @@ export default function EditorPage() {
   return (
     <>
       <Navigation />
+      <center>
+      <Card bg={"dark"} text={"white"} style={{ width: '40rem' }}>
+      <Card.Header as="h5">Visualizing the Self-Assembly of Ions</Card.Header>
+      <Card.Body>
+        <Card.Title>What does this tool do?</Card.Title>
+        <Card.Text>
+          Explanation of this tool.
+          <br />
+          Explanation of this tool.
+        </Card.Text>
+        <Card.Title>How do I use this tool?</Card.Title>
+        <Card.Text>
+          Instructions on how to create a structure.
+          <br />
+          Instructions on how to create a structure.
+        </Card.Text>
+        <Button variant="primary">Watch the demo video</Button>
+      </Card.Body>
+    </Card>
+    </center>
+    <br />
       <StructureEditor />
       <LoadingButton>Visualize Self-Assembly</LoadingButton>
     </>
