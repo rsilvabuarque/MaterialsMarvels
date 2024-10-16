57c7313eb361144ed11cc64ee679bb4dd99dad0f
Sachin Ramanathan <sachinr.exe@gmail.com>
2024-10-16 09:53:55 -0700

    Skip step 10 too due to inaccuracies

--- frontend/app/visualization/VariablePlot.js
+++ frontend/app/visualization/VariablePlot.js
@@ -93,7 +93,7 @@ export default function VariablePlot({ visualId, sliderValue, variableIndex, var
         if (insideData) {
             let columns = line.trim().split(/\s+/);
             let step = parseInt(columns[0]);
-            if (step === 0) return;  // Skip this iteration if step is 0
+            if (step === 0 || step === 10) return;  // Skip this iteration if step is 0 or 10
             let variableValue = parseFloat(columns[variableIndex]);
 
             let color = currentPhase === 'minimization' ? minimizationColor : heatingColor;
